7b4c301a7324977bc3097e7acb49859304b40112
Jijeesh <jijeesh.ka@gmail.com>
2019-05-27 14:21:03 +0530

    kubernetes manifest added

---
+++ kubernetes/run.sh
@@ -0,0 +1,30 @@
+
+kubectl -n hadoop-spark create -f hadoop-env-configmap.yaml -f sparkmaster-worker-configmap.yaml
+
+kubectl -n hadoop-spark create -f redis-deployment.yaml -f redis-service.yaml
+
+kubectl -n hadoop-spark create -f sparkmaster-StatefulSet.yaml -f sparkmaster-service.yaml
+
+
+kubectl -n hadoop-spark create -f worker-StatefulSet-autoscale.yaml
+
+kubectl -n hadoop-spark create -f namenode-StatefulSet.yaml -f namenode-service.yaml
+
+
+
+kubectl -n hadoop-spark create -f datanode-StatefulSet.yaml -f datanode-service.yaml
+
+
+
+kubectl -n hadoop-spark create -f resourcemanager-StatefulSet.yaml -f resourcemanager-service.yaml
+
+
+
+kubectl -n hadoop-spark create -f nodemanager1-StatefulSet.yaml  -f nodemanager1-service.yaml
+
+
+
+
+kubectl -n hadoop-spark create -f historyserver-StatefulSet.yaml -f historyserver-service.yaml
+
+ kubectl -n hadoop-spark create -f hue-deployment.yaml -f hue-service.yaml
